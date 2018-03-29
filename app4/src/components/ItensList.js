a52dbb6ecb4d4753bdec316bdf6ba6a0babfa88c
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-03-29 17:47:08 -0300

    utilizando requisições http

--- app4/src/components/ItensList.js
+++ app4/src/components/ItensList.js
@@ -4,8 +4,16 @@ import {
   View
 } from 'react-native';
 import Itens from './Itens.js'
+import axios from 'axios';
 
 export default class ItensList extends Component<Props> {
+
+	componentWillMount() {
+		axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+			 .then((response) => {console.log(response); })
+			 .catch(() => {console.log('erro ao recuperar os dados'); });
+		//http://faus.com.br/recursos/c/dmairr/api/itens.html
+	}
 
 	render() {
 		return(
