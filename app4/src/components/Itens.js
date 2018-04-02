edc4b867472321b54761badf69c3ce9d3e4b6a81
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-04-02 19:13:16 -0300

    recuperando propriedades dos itens recebidos pelo axios

--- app4/src/components/Itens.js
+++ app4/src/components/Itens.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
 export default class Itens extends Component <Props> {
 
 	render() {
 		return(
-				<Text> Testando Item</Text>
+			<View>
+				<Image style={{ width:100, height: 100 }} source={{uri: this.props.item.foto}}/>
+				<Text>{this.props.item.titulo}</Text>
+				<Text>{this.props.item.local_anuncio}</Text>
+			</View>
 			);
 	}
 }
--- app4/src/components/ItensList.js
+++ app4/src/components/ItensList.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  ScrollView
 } from 'react-native';
 import Itens from './Itens.js'
 import axios from 'axios';
 
 export default class ItensList extends Component<Props> {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {itensList: [] };
+		this.setState = this.setState.bind(this);
+	}
+
+
 	componentWillMount() {
 		axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-			 .then((response) => {console.log(response); })
+			 .then((response) => {this.setState({itensList: response.data}); })
 			 .catch(() => {console.log('erro ao recuperar os dados'); });
 		//http://faus.com.br/recursos/c/dmairr/api/itens.html
 	}
 
 	render() {
 		return(
-			<View>	
-				<Itens></Itens>
-				<Itens></Itens>
-				<Itens></Itens>
-			</View>
+			<ScrollView>	
+				{ this.state.itensList.map((item) => {return(<Itens key={item.titulo} item={item} />)} ) }
+			</ScrollView>
 			
 		);
 	}
+
 
 }
