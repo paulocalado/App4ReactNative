06479e45bb292849452ebcfbd457f9b7b806349a
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-04-03 12:01:19 -0300

    Ajustes no layout e interação com os itens

--- app4/src/components/Itens.js
+++ app4/src/components/Itens.js
@@ -2,18 +2,37 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  Image
+  Image,
+  StyleSheet,
+  TouchableOpacity
 } from 'react-native';
 
 export default class Itens extends Component <Props> {
 
-	render() {
-		return(
-			<View>
-				<Image style={{ width:100, height: 100 }} source={{uri: this.props.item.foto}}/>
-				<Text>{this.props.item.titulo}</Text>
-				<Text>{this.props.item.local_anuncio}</Text>
-			</View>
-			);
+	itemDetail(item){
+		alert('Valor R$'+item.valor);
 	}
-}
+
+	render() {
+		const {container, textStyle} = styles;
+		return(
+			<TouchableOpacity style={container} onPress={() => {this.itemDetail(this.props.item)}}>
+				<Image style={{ width:100, height: 100, margin:5 }} source={{uri: this.props.item.foto}}/>
+				<Text style={textStyle}>{this.props.item.titulo}</Text>
+				<Text>{this.props.item.local_anuncio}</Text>
+			</TouchableOpacity>
+			);
+		}
+	}
+
+const styles = StyleSheet.create( {
+
+	container: {
+		margin:30,
+		backgroundColor: '#B64107'
+	},
+	textStyle: {
+		color: '#fff',
+		fontWeight: 'bold'
+	}
+});
